eec9357a9ef8892fe9ece43e2c9b775aa66749eb
Kritika <24kritikasharma24@gmail.com>
2021-05-05 20:10:31 +0530

    added radio buttons for input and updated output frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
-from tkinter import BOTH
+from tkinter import BOTH, StringVar
+from PIL import Image, ImageTk
 
 # defining main window
 root = tkinter.Tk()
@@ -29,5 +30,16 @@ submit = tkinter.Button(input_frame, text='Submit')
 name.grid(row=0, column=0, padx=10, pady=10)
 submit.grid(row=0, column=1, padx=10, pady=10, ipadx=20)
 
+# radio buttons
+case_style = StringVar()
+case_style.set('normal')
+normal_button = tkinter.Radiobutton(input_frame, text='Normal Case',variable = case_style, value='normal', bg=root_color)
+upper_button = tkinter.Radiobutton(input_frame, text='Upper Case',variable = case_style, value='upper', bg=root_color)
+normal_button.grid(row=1, column=0, padx=2, pady=2)
+upper_button.grid(row=1, column=1, padx=2, pady=2)
+
+myImage = ImageTk.PhotoImage(Image.open('teddy.png'))
+image_label = tkinter.Label(output_frame, image=myImage, bg=output_color)
+image_label.pack()
 
 root.mainloop()
